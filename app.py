badd82be238ae2573d404939efc5d2c849334470
Davi Leal <odavileal77@gmail.com>
2026-04-02 15:54:14 -0300

    subindo a página teste do front

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ def home():
 def calcular():
     sm = int(request.form.get("salario") or 0)
 
+    
+
     salario_anual = sm * 12
     deducao_salario = eid(sm)
     salario_apos_deducao = salario_anual - deducao_salario
@@ -32,12 +34,26 @@ def calcular():
     previdencia = prev(sm)
     seguro_de_saude = saude(sm)
     seguro_desemprego = seg(sm)
-
     deducao_basica = bd(sm)
     renda_tributavel = rt(sm)
-
     imposto = imp(sm)
     residencia = resimp(sm)
+
+    if sm < 95000:
+        return render_template(
+            "index.html",
+            erro="Salário deve ser maior que 95.000 ienes, caso contrário peça insenção a sua prefeitura ",
+            salario_anual=0,
+            deducao_salario=0,
+            salario_apos_deducao=0,
+            previdencia=0,
+            seguro_de_saude=0,
+            seguro_desemprego=0,
+            deducao_basica=0,
+            renda_tributavel=0,
+            imposto=0,
+            residencia=0,
+        )
 
     return render_template(
         "index.html",
@@ -56,3 +72,5 @@ def calcular():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+app.run(debug=True)
